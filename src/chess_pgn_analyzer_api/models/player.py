0b516854e9963fb977ef442606d27de4e21913a2
tadeasf <business@tadeasfort.com>
2024-09-17 01:06:09 +0200

    refactor(player.py): add new fields to Player model for better representation of player data

--- src/chess_pgn_analyzer_api/models/player.py
+++ src/chess_pgn_analyzer_api/models/player.py
@@ -1,8 +1,22 @@
 from sqlmodel import SQLModel, Field
+from datetime import datetime
 from typing import Optional
 
 
 class Player(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True)
-    last_analyzed: Optional[str] = None
+    player_id: int
+    title: Optional[str] = None
+    status: str
+    name: Optional[str] = None
+    avatar: Optional[str] = None
+    location: Optional[str] = None
+    country: str
+    joined: datetime
+    last_online: datetime
+    followers: int
+    is_streamer: bool
+    twitch_url: Optional[str] = None
+    fide: Optional[int] = None
+    last_updated: datetime = Field(default_factory=datetime.utcnow)
