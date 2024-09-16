55f5958ec2883cb943460e97ffcee60f129d5353
tadeasf <business@tadeasfort.com>
2024-09-17 01:05:30 +0200

    refactor(database.py): remove unused init_db function to clean up code and improve readability

--- src/chess_pgn_analyzer_api/database.py
+++ src/chess_pgn_analyzer_api/database.py
@@ -19,12 +19,6 @@ engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
-
