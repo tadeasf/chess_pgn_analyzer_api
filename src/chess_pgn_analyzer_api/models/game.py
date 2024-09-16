1a6be8399dc6fa61440b2a97838bf8765195c8b0
tadeasf <business@tadeasfort.com>
2024-09-17 01:06:04 +0200

    feat(models): add new fields to Game model for move analysis and game metadata
    feat(models.py): add set_analyzed_status method to set the analyzed status based on analysis_result data in the model

--- src/chess_pgn_analyzer_api/models/game.py
+++ src/chess_pgn_analyzer_api/models/game.py
@@ -1,11 +1,39 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import datetime
+import json
 
 
 class Game(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     player_id: int = Field(foreign_key="player.id")
     game_id: str = Field(index=True)
+    url: str
     pgn: str
-    analyzed: bool = False
+    analyzed: bool = Field(default=False)
     analysis_result: Optional[str] = None
+    moves_analyzed: bool = Field(default=False)
+    move_analysis: Optional[str] = None  # Add this line
+    white_username: str
+    black_username: str
+    white_rating: int
+    black_rating: int
+    white_result: str
+    black_result: str
+    start_time: Optional[datetime]
+    end_time: datetime
+    time_control: str
+    rules: Optional[str]
+    eco: Optional[str]
+    tournament: Optional[str]
+    match: Optional[str]
+
+    def set_analyzed_status(self):
+        if self.analysis_result:
+            try:
+                analysis_data = json.loads(self.analysis_result)
+                self.analyzed = bool(analysis_data) and any(analysis_data.values())
+            except json.JSONDecodeError:
+                self.analyzed = False
+        else:
+            self.analyzed = False
